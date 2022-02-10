d1368268f5ed9c9d9a5d38395a5c4d710fed1e4f
Dorota Borowska <youcode2021db@gmail.com>
2022-02-10 21:19:09 +0100

    First init

---
+++ src/Form/index.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./style.css";
+import { currencies } from '../currencies';
+
+const Form = (calculateResult, result) => {         //it only works without curly brackets in arguments
+    const [currency, setCurrency] = useState([]);
+    const [amount, setAmount] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        calculateResult(currency, amount);
+    }
+
+    return (
+
+        <form onSubmit={onSubmit} >
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">Przelicznik walut</legend>
+                <p>
+                    <label>
+                        Kwota w PLN*:
+                        <input
+                            value={amount}
+                            onChange={({ target }) => setAmount(target.value)}
+                            placeholder="Wpisz kwotę w PLN"
+                            type="number"
+                            step="0.01"
+                            required />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Wybierz walutę:
+                        <select
+                            value={currency}
+                            onChange={({ target }) => setCurrency(target.value)}
+                        >
+                            {currencies.map((currency => (
+                                <option
+                                    key={currency.short}
+                                    value={currency.short}
+                                >
+                                    {currency.name}
+                                </option>
+                            )))}
+                        </select>
+                    </label>
+                </p>
+                <p>
+                    Aktualny kurs: <strong >{calculateResult.rate}</strong>
+                </p>
+            </fieldset>
+            <p className="form__result">
+                Otrzymasz: <strong >{result.targetAmount}</strong>
+            </p>
+            <p>
+                <button type="submit" className="form__button">Przelicz</button>
+            </p>
+            <p>
+                <button
+                    type="reset"
+                    className="form__button form__button--reset"
+                >Wyczyść</button>
+            </p>
+        </form>
+    );
+};
+
+export default Form;
---
+++ src/currencies.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const currencies = [
+    {
+        name: "Euro",
+        short: "EUR",
+        rate: 4.5316,
+    },
+    {
+        name: "Dolar amerykański",
+        short:"USD",
+        rate: 3.7374,
+    },
+{
+        name: "Funt brytyjski",
+        short: "GPB",
+        rate: 5.2571,
+    }
+]
