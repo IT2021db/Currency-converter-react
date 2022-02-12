73e4108e09053b3f5f2d559921413c2c95b1c107
Dorota Borowska <youcode2021db@gmail.com>
2022-02-12 19:17:52 +0100

    react component Result created

---
+++ src/Form/Result/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Result = ({ result }) => (
+    <p>
+        {!!result && (
+            <>
+                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
+                <strong>
+                    {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+                </strong>
+            </>
+        )}
+    </p>
+);
+
+export default Result;
