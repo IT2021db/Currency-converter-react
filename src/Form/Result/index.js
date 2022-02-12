fb8489bd7d7631d44ace94f19399fa100fb8e98a
Dorota Borowska <youcode2021db@gmail.com>
2022-02-12 20:40:26 +0100

    className result added

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,13 +1,17 @@
 import React from "react";
+import "./style.css";
 
 const Result = ({ result }) => (
-    <p>
+    <p className="result">
         {!!result && (
             <>
                 {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
                 <strong>
                     {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
                 </strong>
+                <p>
+                    Kurs&nbsp;{result.currency}&nbsp;wynosi:&nbsp;{result.rate}PLN
+                </p>
             </>
         )}
     </p>
