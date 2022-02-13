0cfb0114a94a863f6c83eb5b9885180e65eeaf7a
Dorota Borowska <youcode2021db@gmail.com>
2022-02-13 23:06:57 +0100

    capital letters in currency changed

--- src/currencies.js
+++ src/currencies.js
@@ -1,16 +1,16 @@
 export const currencies = [
     {
-        name: "Euro",
+        name: "euro",
         short: "EUR",
         rate: 4.5316,
     },
     {
-        name: "Dolar amerykański",
-        short:"USD",
+        name: "dolar amerykański",
+        short: "USD",
         rate: 3.7374,
     },
-{
-        name: "Funt brytyjski",
+    {
+        name: "funt brytyjski",
         short: "GPB",
         rate: 5.2571,
     }
