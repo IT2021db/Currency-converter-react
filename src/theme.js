c16716a587875264f3f22de54a6cada1195a14ed
Dorota Borowska <youcode2021db@gmail.com>
2022-02-26 12:15:36 +0100

    themeProvider implemented

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    color: {
+        white: "#FFF",
+        clearDay: "#E9FFFD",
+        hintOfRed: "#FBF9F9",
+        grey: "#808080",
+        greyRomantic: "#4c6b6b",
+        spray: "#79DEEC",
+        teal: "#008080",
+        thunderbird: "#D31212",
+    },
+};
