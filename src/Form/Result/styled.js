5cdb6df4810178733af7996dc480e0e32972beb2
Dorota Borowska <youcode2021db@gmail.com>
2022-02-26 13:36:08 +0100

    code formatted

--- src/Form/Result/styled.js
+++ src/Form/Result/styled.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
-export const Info=styled.p`
+export const Info = styled.p`
     font-size: 15px;
-    color: ${({theme})=>theme.color.grey};
+    color: ${({ theme }) => theme.color.grey};
 `;
 
-export const Expected=styled.strong`
+export const Expected = styled.strong`
     font-size:25px;
-    color: ${({theme})=>theme.color.thunderbird};
+    color: ${({ theme }) => theme.color.thunderbird};
 `;
--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,4 +1,4 @@
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Wrapper = styled.form`
     max-width: 500px;
@@ -6,23 +6,23 @@ export const Wrapper = styled.form`
   `;
 
 export const Legend = styled.legend`
-    background-color:  ${({theme})=>theme.color.spray};
+    background-color:  ${({ theme }) => theme.color.spray};
     border-radius: 5px;
     padding: 5px;
-    color:  ${({theme})=>theme.color.teal};
+    color:  ${({ theme }) => theme.color.teal};
     font-weight: 700;
     text-align: left;
     font-size: 20px;
 `;
 
 export const Fieldset = styled.fieldset`
-    background-color: ${({theme})=>theme.color.clearDay};
+    background-color: ${({ theme }) => theme.color.clearDay};
     border:none;
     margin: 15px auto;
     border-radius: 5px;
     max-width: 500px;
     padding: 10px 20px 20px 20px;
-    color:   ${({theme})=>theme.color.teal};
+    color:   ${({ theme }) => theme.color.teal};
     font-weight: bold;
 `;
 
@@ -34,30 +34,30 @@ export const Field = styled.input`
     `;
 
 export const ResultBlock = styled.p`
-    background-color:  ${({theme})=>theme.color.clearDay};
+    background-color:  ${({ theme }) => theme.color.clearDay};
     padding: 20px 15px;
     font-weight: 700;
     font-size: 20px;
     border-radius: 5px;
     text-align: center;
-    color:  ${({theme})=>theme.color.teal};
+    color:  ${({ theme }) => theme.color.teal};
     `;
 
 export const Button = styled.button`
     padding: 15px;
     border: none;
     border-radius: 5px;
-    background-color:  ${({theme})=>theme.color.spray};
+    background-color:  ${({ theme }) => theme.color.spray};
     width: 100%;
-    color:  ${({theme})=>theme.color.teal};
+    color:  ${({ theme }) => theme.color.teal};
     font-size: 20px;
     font-weight:bold;
 
     &:hover {
-        background-color: ${({theme})=>theme.color.teal};
-        color: ${({theme})=>theme.color.white};
+        background-color: ${({ theme }) => theme.color.teal};
+        color: ${({ theme }) => theme.color.white};
     }
 `;
- 
+
 
 
