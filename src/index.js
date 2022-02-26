a1f94f9383da89fa228f0a8b3aa41601fb716671
Dorota Borowska <youcode2021db@gmail.com>
2022-02-26 13:39:18 +0100

    GlobalStyle implemented

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
-import './index.css';
+import { GlobalStyle } from "./GlobalStyle";
 import App from './App';
-import {theme} from "./theme";
+import { theme } from "./theme";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
