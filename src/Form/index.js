ea393c3814266a9d62f6a08307efc7bea811d987
Dorota Borowska <youcode2021db@gmail.com>
2022-03-06 07:33:28 +0100

    ratesApi.state cases resolved

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,68 +1,77 @@
 import React, { useState } from "react";
-import { currencies } from '../currencies';
 import Result from "./Result";
 import { Clock } from "./Clock";
-import { Wrapper, Legend, Fieldset, Button, ResultBlock, Field } from "./styled";
+import { Wrapper, Legend, Fieldset, Button, ResultBlock, Field, InformationApi } from "./styled";
 
-
-const Form = ({ calculateResult, result, ratesData }) => {
-    const [currency, setCurrency] = useState("PLN");
+const Form = ({ calculateResult, result, ratesApi }) => {
+    const [currency, setCurrency] = useState("EUR");
     const [amount, setAmount] = useState("");
 
     const onSubmit = (event) => {
         event.preventDefault();
         calculateResult(currency, amount);
     }
+
     return (
         <Wrapper onSubmit={onSubmit}>
-            <Fieldset>
-                <Legend >Przelicznik walut</Legend>
-                <Clock />
-                <p>
-                    <label>
-                        Kwota w PLN*:{" "}
-                        <Field
-                            value={amount}
-                            onChange={({ target }) => setAmount(target.value)}
-                            placeholder="Wpisz kwotę w PLN"
-                            type="number"
-                            step="0.01"
-                            required />
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        Wybierz walutę:{" "}
-                        <Field                      //tutaj sie wykłada app
-                            as="select"
-                            value={currency}
-                            onChange={({ target }) => setCurrency(target.value)}
-                        >
-                            {Object.keys(ratesData.rates).map(((currency) => (
-                                <option
-                                    key={currency}
-                                    value={currency}
-                                >
-                                    {currency}
-
-                                    {/* {currencies.map((currency => (
-                                <option
-                                    key={currency.short}
-                                    value={currency.short}
-                                >
-                                    {currency.name}  */}
-
-                                </option>
-                            )))}
-                        </Field>
-                    </label>
-                </p>
-            </Fieldset>
-            <ResultBlock>
-                <p> <strong>Otrzymasz:</strong></p>
-                <Result result={result} />
-            </ResultBlock>
-            <Button type="submit">Przelicz</Button>
+            {ratesApi.state === "loading"
+                ? (
+                    <InformationApi>Trwa ładowanie danych z Europejskigo Banku Centralnego</InformationApi>
+                )
+                : (
+                    ratesApi.state === "error"
+                        ? (
+                            <InformationApi>erroor Banku Centralnego</InformationApi>
+                        )
+                        : (
+                            <>
+                                <Fieldset>
+                                    <Legend >Przelicznik walut</Legend>
+                                    <Clock />
+                                    <p>
+                                        <label>
+                                            Kwota w PLN*:{" "}
+                                            <Field
+                                                value={amount}
+                                                onChange={({ target }) => setAmount(target.value)}
+                                                placeholder="Wpisz kwotę w PLN"
+                                                type="number"
+                                                step="0.01"
+                                                required />
+                                        </label>
+                                    </p>
+                                    <p>
+                                        <label>
+                                            Wybierz walutę:{" "}
+                                            <Field
+                                                as="select"
+                                                value={currency}
+                                                onChange={({ target }) => setCurrency(target.value)}
+                                            >
+                                                {Object.keys(ratesApi.rates).map(((currency) => (
+                                                    <option
+                                                        key={currency}
+                                                        value={currency}
+                                                    >
+                                                        {currency}
+                                                    </option>
+                                                )))}
+                                            </Field>
+                                        </label>
+                                    </p>
+                                </Fieldset>
+                                <ResultBlock>
+                                    <p> <strong>Otrzymasz:</strong></p>
+                                    <Result
+                                        result={result}
+                                        ratesApi={ratesApi}
+                                    />
+                                </ResultBlock>
+                                <Button type="submit">Przelicz</Button>
+                            </>
+                        )
+                )
+            }
         </Wrapper>
     )
 };
