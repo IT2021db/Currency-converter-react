acc8520923a8e92648a540fbc3687dc6cd63f364
Dorota Borowska <youcode2021db@gmail.com>
2024-01-12 17:49:27 +0100

    new Date added to fix date format

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Info, Expected } from "./styled";
 
-const Result = ({ result, exchangeRates }) => (
-    <div>
-        {!!result && (
-            <>
-                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
-                <Expected>
-                    {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
-                </Expected>
-                <Info>
-                    Kurs&nbsp;{result.currency}&nbsp;wynosi:&nbsp;{result.exchangeRate.toFixed(3)}&nbsp;PLN<br />
-                    z dnia:&nbsp;{exchangeRates.date}
-                </Info>
-            </>
-        )}
-    </div>
-);
+const Result = ({ result, exchangeRates }) => {
+    const date = new Date(exchangeRates.date);
+    const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
 
+    return (
+        <div>
+            {!!result && (
+                <>
+                    {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
+                    <Expected>
+                        {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+                    </Expected>
+                    <Info>
+                        Kurs&nbsp;{result.currency}&nbsp;wynosi:&nbsp;{result.exchangeRate.toFixed(3)}&nbsp;PLN<br />
+                        z dnia:&nbsp;{formattedDate}
+                    </Info>
+                </>
+            )}
+        </div>
+    );
+};
 export default Result;
